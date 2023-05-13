cd7874c5ec2b45618ac8d7bf7b3c45b36322a3e2
Ervin Szilágyi <ervin_szilagyi@outlook.com>
2023-05-13 19:00:29 +0000

    cert-ripper-go 0.4.2

--- Formula/cert-ripper-go.rb
+++ Formula/cert-ripper-go.rb
@@ -2,11 +2,11 @@ class CertRipperGo < Formula
   desc "Print, extract and validate TLS certificate chains for domains and URLs"
   homepage "https://github.com/recon-tools/cert-ripper-go"
   license "MIT"
-  version "0.3.0"
+  version "0.4.2"
 
   if Hardware::CPU.intel?
-    url "https://github.com/recon-tools/cert-ripper-go/releases/download/v0.3.0/cert-ripper-0.3.0-darwin-amd64.tar.gz"
-    sha256 "3ba0bcb74fd86208129a909477a1a3f05b5157c0c275c2e68751811d0fdcd42f"
+    url "https://github.com/recon-tools/cert-ripper-go/releases/download/v0.4.2/cert-ripper-0.4.2-darwin-amd64.tar.gz"
+    sha256 "eb1dedd54a11df0d0332f9296779900fa42313a1192591a787330ed1f31f4492"
   elsif Hardware::CPU.arm?
     url "https://github.com/recon-tools/cert-ripper-go/releases/download/v0.3.0/cert-ripper-0.3.0-darwin-arm64.tar.gz"
     sha256 "93ae897f6de86f5ec35442287830e92a928174885a6606a2a8da377a0612c201"
