ba103e3b208635cf93bf3cc76768e187a9789c78
Ervin Szilagyi <ervin_szilagyi@outlook.com>
2023-05-13 22:09:41 +0300

    Bump ARM version.

--- Formula/cert-ripper-go.rb
+++ Formula/cert-ripper-go.rb
@@ -8,8 +8,8 @@ class CertRipperGo < Formula
     url "https://github.com/recon-tools/cert-ripper-go/releases/download/v0.4.2/cert-ripper-0.4.2-darwin-amd64.tar.gz"
     sha256 "eb1dedd54a11df0d0332f9296779900fa42313a1192591a787330ed1f31f4492"
   elsif Hardware::CPU.arm?
-    url "https://github.com/recon-tools/cert-ripper-go/releases/download/v0.3.0/cert-ripper-0.3.0-darwin-arm64.tar.gz"
-    sha256 "93ae897f6de86f5ec35442287830e92a928174885a6606a2a8da377a0612c201"
+    url "https://github.com/recon-tools/cert-ripper-go/releases/download/v0.4.2/cert-ripper-0.4.2-darwin-arm64.tar.gz"
+    sha256 "64ee7b15d74c8ed69172e36ad54c0b9af9f858cac879107c89a9356b81029571"
   else
     odie "Unsupported architecture"
   end
