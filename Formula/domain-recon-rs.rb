316360a096d199993697e961bb4239a8302f2b0b
Ervin Szilágyi <ervin_szilagyi@outlook.com>
2023-06-05 23:16:20 +0000

    domain-recon-rs 1.3.0

--- Formula/domain-recon-rs.rb
+++ Formula/domain-recon-rs.rb
@@ -1,10 +1,10 @@
 class DomainReconRs < Formula
   desc "Discover domain names based on SSL certificates."
   homepage "https://github.com/recon-tools/domain-recon-rs"
-  url "https://github.com/recon-tools/domain-recon-rs/releases/download/v1.2.2/domain-recon-v1.2.2.apple-darwin.tar.gz"
-  sha256 "c4c34eb858dd16768c70f5dc42744e303408eb838112b8eb2155ce7a217a623b"
+  url "https://github.com/recon-tools/domain-recon-rs/releases/download/v1.3.0/domain-recon-v1.3.0.apple-darwin.tar.gz"
+  sha256 "e055d921bc4d658c208b684dc05de670f3f482ed3bab8c7775c15c81d6a87312"
   license "MIT"
-  version "1.2.2"
+  version "1.3.0"
 
   def install
     bin.install "domain-recon"
